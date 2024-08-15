478bcf02c30ebe27425c01d6fdea18db870a75da
Vishwajeet <manevishwajeet1@gmail.com>
2024-08-15 15:50:24 +0200

    gitbook sync test

---
+++ openmath-rust/src/main.rs
@@ -0,0 +1,30 @@
+/*
+    * OpenMath - A Rust library for OpenMath
+    * Copyright (C) 2024 OpenMath Development Team
+    *
+    * @file src/main.rs
+    * @brief Main file for the OpenMath Rust library
+    * @date 2024-08-15
+    *
+    * @author Vishwajeet Mane
+    * @version 0.1
+    * @date 2024-01-01
+    *
+    * @todo Everything
+    * @bug None
+    * @warning None
+*/
+
+use std;
+use ferris_says;
+
+fn main() {
+    // let stdout = std::io::stdout();
+    // let message = String::from("Hello, World!");
+    // let width = message.chars().count();
+
+    // let mut writer = std::io::BufWriter::new(stdout.lock());
+    // ferris_says::say(&message, width, &mut writer).unwrap();
+
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+}
