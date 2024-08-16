97fa43b559178047d92abde3e4f4919fcd9e8241
Vishwajeet <manevishwajeet1@gmail.com>
2024-08-16 12:14:56 +0200

    Main File Changes

---
+++ openmaths-rust/src/main.rs
@@ -0,0 +1,30 @@
+/*
+    * OpenMaths - A Rust library for OpenMaths
+    * Copyright (C) 2024 OpenMaths Development Team
+    *
+    * @file src/main.rs
+    * @brief Main file for the OpenMaths Rust library
+    * @date 2024-08-15
+    *
+    * @author Vishwajeet Mane
+    * @version 0.1
+    * @date 2024-01-01
+    *
+    * @todo Everything
+    * @bug None
+    * @warning None
+*/
+
+const PACKAGE_NAME: &str = "openmaths";
+const PACKAGE_VERSION: &str = "0.0.1";
+
+fn main() {
+    // let stdout = std::io::stdout();
+    // let message = String::from("Hello, World!");
+    // let width = message.chars().count();
+
+    // let mut writer = std::io::BufWriter::new(stdout.lock());
+    // ferris_says::say(&message, width, &mut writer).unwrap();
+
+    println!("{0}::{1} up and running", PACKAGE_NAME, PACKAGE_VERSION);
+}
